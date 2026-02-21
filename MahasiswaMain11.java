8bd1578abf3100f7b37d5008d3b2e7fed356d9f8
melizahaiziK <melizaha1zik@gmail.com>
2026-02-21 22:34:18 +0700

    Percobaan 3

--- MahasiswaMain11.java
+++ MahasiswaMain11.java
@@ -12,5 +12,8 @@ public class MahasiswaMain11 {
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
 
+        Mahasiswa11 mhs2 = new Mahasiswa11("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
